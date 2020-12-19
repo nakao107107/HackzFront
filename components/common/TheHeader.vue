f5c1c71443db090254e4dd4d0fdf7f94272b5ce4
nakao107107 <imakeanewstory@gmail.com>
2020-12-19 12:45:17 +0900

    なんとなくheader作成

---
+++ components/common/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header d-flex justify-content-end align-items-center p-4">
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .header {
+    background: #2E2E2E;
+    border: 1px solid #1A1A1A;
+  }
+
+  .icon {
+    width: 30px;
+    height: 30px;
+    border-radius: 7px;
+    background: $primary;
+    cursor: pointer;
+  }
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
-    <Nuxt />
+    <TheHeader class="header"/>
+    <Nuxt/>
   </div>
 </template>
+
+<script>
+  import TheHeader from '~/components/common/TheHeader.vue'
+  export default {
+    components: {
+      TheHeader
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .header {
+    height: 50px;
+    width: 100%;
+    position: fixed;
+  }
+  .content {
+    height: calc(100vh - 80px);
+  }
+</style>
