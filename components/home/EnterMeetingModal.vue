22513e3673f7daa165d2688739a787ba4c15fc5c
nakao107107 <imakeanewstory@gmail.com>
2020-12-20 07:38:39 +0900

    参加モーダル修正

--- components/home/EnterMeetingModal.vue
+++ components/home/EnterMeetingModal.vue
@@ -10,11 +10,24 @@
     <div class="p-4">
       <h5 class="font-weight-bold">ミーティングに参加</h5>
       <div class="form-group">
-        <input class="form-control" placeholder="会議室ID" v-model="meetingId"/>
+        <input
+          class="form-control"
+          :class="{'error': status.isError}"
+          placeholder="会議室ID"
+          v-model="meetingId"
+        />
+        <span class="text-danger mb-2" v-if="status.isError">入室に失敗しました。MeetingIDを確認して下さい。</span>
       </div>
       <div class="d-flex justify-content-end">
         <button class="btn btn-outline-light mr-3" @click="close">キャンセル</button>
-        <button class="btn btn-secondary" @click="enter">参加</button>
+        <button
+          class="btn btn-secondary"
+          @click="enter"
+          :disabled="status.isEntering"
+        >
+          <i class="fas fa-spinner fa-spin mr-1" v-if="status.isEntering"></i>
+          参加
+        </button>
       </div>
     </div>
   </BModal>
@@ -30,17 +43,35 @@
     },
     data(){
       return {
+        status: {
+          isError: false,
+          isEntering: false
+        },
         meetingId: ''
       }
     },
     methods: {
       async enter(){
-        await this.$store.dispatch('meetings/detail/attend', this.meetingId)
-        this.$router.push(`/meetings/${this.meetingId}/view`)
+        this.status.isEntering = true
+        try {
+          await this.$store.dispatch('meetings/detail/attend', this.meetingId)
+          this.$router.push(`/meetings/${this.meetingId}/view`)
+        }catch(e){
+          this.status.isError = true
+        }
+        this.status.isEntering = false
       },
       close(){
+        this.meetingId = ''
+        this.status.isError = false
         this.$emit('close-modal')
       }
     }
   }
 </script>
+
+<style lang="scss" scoped>
+  .error {
+    border: 1px solid $danger;
+  }
+</style>
